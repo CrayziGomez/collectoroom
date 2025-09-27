cc4e9d675562d7785c475faa9576675131b518b9
Geo Kat <gkccgr@gmail.com>
2025-09-27 19:12:22 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/actions/collection-actions.ts
+++ src/app/actions/collection-actions.ts
@@ -1,7 +1,7 @@
 
 'use server';
 
-import { adminDb, adminStorage } from '@/lib/firebase-admin';
+import { adminDb, adminStorage } from '../../lib/firebase-admin';
 import { v4 as uuidv4 } from 'uuid';
 import { revalidatePath } from 'next/cache';
 
--- src/app/actions/user-actions.ts
+++ src/app/actions/user-actions.ts
@@ -1,7 +1,7 @@
 
 'use server';
 
-import { adminDb, adminStorage } from '@/lib/firebase-admin';
+import { adminDb, adminStorage } from '../../lib/firebase-admin';
 import { FieldValue } from 'firebase-admin/firestore';
 import { revalidatePath } from 'next/cache';
 
