8358f4f1dab57ab37927c298b5aa37a8ca6ebbed
Geo Kat <gkccgr@gmail.com>
2025-09-27 17:51:02 +0000

    set up the env.local with the keys required for the project: // Your web

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,11 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "studio-7145415565-66e7d",
-  "appId": "1:47670381092:web:6e67a8da3455b65518a6b1",
-  "apiKey": "AIzaSyD81Q7lwEparRgB4pRKq5ezOuS2KzrMLMk",
-  "authDomain": "studio-7145415565-66e7d.firebaseapp.com",
-  "messagingSenderId": "47670381092"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
