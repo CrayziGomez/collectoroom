bbcc223c8de58cc9c0ca9bfdb124a5a226df6005
Geo Kat <gkccgr@gmail.com>
2025-09-27 23:48:04 +0000

    try the image in the public folder, called CR_Logo_Gry.png as the logo i

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" aria-label="CollectoRoom Home">
       <Image 
-        src="/images/logo.svg" 
+        src="/CR_Logo_Gry.png" 
         alt="CollectoRoom Logo" 
         width={32} 
         height={32} 
